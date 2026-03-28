859b2b34e8c7be0690dd03892efd2ff06ca562db
Pichu <pichu_2707@hotmail.com>
2026-03-28 16:44:24 +0100

    Subiendo ejercicios realizados de Java

---
+++ desafios/incrementoDecremento.java
@@ -0,0 +1,13 @@
+public class incrementoDecremento {
+	public static void main(String[] args) {
+		int count = 0;
+		count++;
+		count++;
+		count++;
+		count++;
+		count = count * 2;
+		count--;
+		System.out.println("count = " + count);
+}
+	
+}
---
+++ desafios/operadoresAritmeticos.java
@@ -0,0 +1,11 @@
+public class operadoresAritmeticos {
+	public static void main(String[] args) {
+		double a = 5.2;
+		double b = 2.6;
+		double c = a / b;
+		
+		System.out.println("a = "+ a + ", b = " + b + ", c = " +c);
+		
+	}
+	
+}
